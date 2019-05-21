64b30ca06b9d44235ee51b12a46e15fbe4b5149e
Taras Burdeniuk <bur.taras@gmail.com>
2019-05-21 23:14:29 +0300

    fix search from description

--- src/action/searchTodo.js
+++ src/action/searchTodo.js
@@ -1,7 +1,12 @@
 export const SEARCH_TODO = 'SEARCH_TODO';
 
 export const searchTodo = (todoLIst, searchingWord) => {
-    const newTodoList = todoLIst.filter(item => (item.title === searchingWord) && item.title);
+    const newTodoList = todoLIst.filter(item => {
+        const w = item.description.toLowerCase().split(' ');
+
+        return (Object.is(item.title, searchingWord) || w.includes(searchingWord));
+    });
+
     return {
         type: SEARCH_TODO,
         payload: newTodoList
